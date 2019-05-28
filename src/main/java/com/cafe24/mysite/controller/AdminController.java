07ec5e465cb664722e92e42f38f4d9e20f500906
chu1070y <chu1070y@naver.com>
2019-05-28 21:20:49 +0900

    별거없음

--- src/main/java/com/cafe24/mysite/controller/AdminController.java
+++ src/main/java/com/cafe24/mysite/controller/AdminController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.cafe24.mysite.service.AdminService;
 import com.cafe24.mysite.vo.SiteVO;
